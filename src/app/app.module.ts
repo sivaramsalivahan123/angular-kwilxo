45936b05611faf5a63d139f017bfa1ba475ec312
sivaramsalivahan123 <sivaramsalivahan@gmail.com>
2021-02-11 10:09:27 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
@@ -10,15 +9,19 @@ import { ProductAlertComponent } from "./product-alert/product-alert.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CartService } from "./cart.service";
 import { CartComponent } from "./cart/cart.component";
+import { ShippingComponent } from "./shipping/shipping.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
       { path: "product/:productId", component: ProductDetailsComponent },
-      { path: "cart", component: CartComponent }
+      { path: "cart", component: CartComponent },
+      { path: "shipping", component: ShippingComponent }
     ])
   ],
   declarations: [
@@ -27,7 +30,8 @@ import { CartComponent } from "./cart/cart.component";
     ProductListComponent,
     ProductAlertComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [AppComponent],
   providers: [CartService]
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@angular/core";
 import { root } from "rxjs/internal/util/root";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: root
 })
 export class CartService {
   items = [];
-  constructor() {}
+  constructor(private http: HttpClient) {}
   //adding into Cart
   addCart(product) {
     this.items.push(product);
@@ -20,5 +21,8 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+  getShipping() {
+    return this.http.get("./assets/getShipping.json");
   }
 }
